1a199f00609d0487df8398673c285bcec950538d
asauchik <asauchik@gmail.com>
2025-03-12 12:04:25 -0400

    3-12

---
+++ plasmic-init.ts
@@ -0,0 +1,69 @@
+import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
+
+export const PLASMIC = initPlasmicLoader({
+  projects: [
+    {
+      id: "u4Y4MGpSRpZo7PLABQqvDf",
+      token: "wUDJUv689mK51zFqFe56BKKFA4QfSemrqtfaMAznI8cj39Heg5pHiiMgZKuugnvnkn5resBQaSVocJFfu4BtQ",
+    },
+  ],
+
+  // By default Plasmic will use the last published version of your project.
+  // For development, you can set preview to true, which will use the unpublished
+  // project, allowing you to see your designs without publishing.  Please
+  // only use this for development, as this is significantly slower.
+  preview: false,
+});
+
+// You can register any code components that you want to use here; see
+// https://docs.plasmic.app/learn/code-components-ref/
+// And configure your Plasmic project to use the host url pointing at
+// the /plasmic-host page of your nextjs app (for example,
+// http://localhost:3000/plasmic-host).  See
+// https://docs.plasmic.app/learn/app-hosting/#set-a-plasmic-project-to-use-your-app-host
+
+// PLASMIC.registerComponent(...);
+import { HelloWorld } from './components/HelloWorld';
+
+// ...
+
+PLASMIC.registerComponent(HelloWorld, {
+  name: 'HelloWorld',
+  props: {
+    verbose: 'boolean',
+    children: 'slot'
+  }
+});
+
+import { PdfViewer } from "./components/PdfViewer";
+
+// Register the PdfViewer component in Plasmic
+PLASMIC.registerComponent(PdfViewer, {
+  name: "PdfViewer",
+  props: {
+    pdfUrl: {
+      type: "string",
+      defaultValue: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+    },
+    pageNumber: {
+      type: "number",
+      defaultValue: 1,
+    },
+    className: "string",
+  },
+});
+
+
+import TwilioVerify from "./components/TwilioVerify"; // Adjust path if needed
+
+PLASMIC.registerComponent(TwilioVerify, {
+  name: "TwilioVerify",
+  displayName: "Twilio Phone Verification",
+  props: {
+    phoneNumber: {
+      type: "string",
+      defaultValue: "",
+      description: "Phone number to send verification code",
+    },
+  },
+});
